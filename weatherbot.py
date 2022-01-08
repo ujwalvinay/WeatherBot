46e15b1d35be07254599ff943e87fa1c6f3c2b4d
Ujwal <Walkerdoodle7@gmail.com>
2022-01-08 08:36:27 -0800

    Source code updated

--- weatherbot.py
+++ weatherbot.py
@@ -30,5 +30,22 @@ async def on_message(message):
 
     if message.content.startswith('Who made you') or message.content.startswith('who made you'):
         await message.channel.send('A grad student named Ujwal')
+
+#to check weather
+    if message.content.startswith('weather') or message.content.startswith('Weather') :
+        channel = message.channel
+        await channel.send('Enter the city name')
+
+        
+        city = await client.wait_for('message')
+        url = api_adress + city.content
+        json_data = requests.get(url).json()
+        weather_des = json_data['weather'][0]['description']
+        temp = json_data['main']['temp']
+
+
+        await channel.send("In {} it's {} degree Faranheit with {}".format(city.content , temp , weather_des))
+
+
 #accessing token
 my_secret = os.environ['token new']#enter your token here
